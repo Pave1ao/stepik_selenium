0aabafbe49c41a82eb67d39df94f4cce80153abe
Pave1ao <42095574+Pave1ao@users.noreply.github.com>
2019-09-05 14:19:25 +0300

    product page

--- pages/locators.py
+++ pages/locators.py
@@ -7,3 +7,10 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, ".login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, ".register_form")
+
+class ProductPageLocators():
+    ADD_TO_CART_BTN = (By.CSS_SELECTOR, "button.btn-add-to-basket")
+    BOOKNAME = (By.CSS_SELECTOR, "div.product_main>h1")
+    PRICE = (By.CSS_SELECTOR, "div.product_main>p.price_color")
+    BOOKNAME_ALERT = (By.CSS_SELECTOR, ".alert:first-child .alertinner")
+    PRICE_ALERT = (By.CSS_SELECTOR, ".alert:last-child .alertinner")
---
+++ pages/product_page.py
@@ -0,0 +1,28 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+class ProductPage(BasePage):
+    def add_product_to_cart(self):
+        add_btn = self.browser.find_element(*ProductPageLocators.ADD_TO_CART_BTN)
+        add_btn.click()
+
+    def should_be_add_to_cart_btn(self):
+        assert self.is_element_present(*ProductPageLocators.ADD_TO_CART_BTN), "No 'Add to cart' button"
+
+    def should_be_alert_contains_bookname(self):
+        text = self.browser.find_element(*ProductPageLocators.BOOKNAME_ALERT).text
+        bookname = self.get_bookname()
+        assert bookname in text, f"No such bookname in alert. Except '{bookname}' in '{text}'"
+
+    def should_be_alert_contain_price(self):
+        text = self.browser.find_element(*ProductPageLocators.PRICE_ALERT).text
+        price = self.get_price()
+        assert price in text, f"No such price in alert. Ecxept '{price}' in '{text}'"
+
+    def get_price(self):
+        price = self.browser.find_element(*ProductPageLocators.PRICE).text
+        return price
+
+    def get_bookname(self):
+        bookname = self.browser.find_element(*ProductPageLocators.BOOKNAME).text
+        return bookname
